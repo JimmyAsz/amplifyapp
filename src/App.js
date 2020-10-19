11d1b0c011b1e9f0d2084bfecb59f2200b93d9c0
Jaime Atriano <cod.jsz.deb@gmail.com>
2020-10-19 16:49:34 -0500

    Auth v2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-
 function App() {
-    return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Hello from V2</h1>
-                </header>
-             <AmplifySignOut />
-
-            </div>
-            );
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
 }
 
 export default withAuthenticator(App);
